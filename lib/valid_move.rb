5fd68fb5b8c5babab38ff48b0092a3cdfc308c2a
Mazen AlSwar <mazenswar@protonmail.com>
2018-10-18 20:34:00 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -12,5 +12,5 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if !position_taken(board, index) &&  index.between
+  if !position_taken(board, index) &&  index.between?(0, 8)
     
